16eb15aba5895a8d53f0e70abe51d6a6c14c402a
Juntao Han <499445428@qq.com>
2018-01-28 15:27:18 +0800

    Add heap

---
+++ Heap/src/pers/mingshan/heap/IHeap.java
@@ -0,0 +1,59 @@
+package pers.mingshan.heap;
+
+/**
+ * 堆 接口
+ * 
+ * @author mingshan
+ *
+ */
+public interface IHeap<T extends Comparable<T>> {
+
+    /**
+     * 将一个值添加到堆中
+     * 
+     * @param value 要添加的值
+     */
+    void add(T value);
+
+    /**
+     * 获取Head value
+     * 
+     * @return head
+     */
+    T getHeadValue();
+
+    /**
+     * 移除某个元素
+     * 
+     * @param value
+     * @return 移除的元素
+     */
+    T remove(T value);
+
+    /**
+     * 清空堆
+     */
+    void clear();
+
+    /**
+     * 判断堆中是否包含某个元素
+     * 
+     * @param value
+     * @return 如果包含，返回{@code true}，否则返回{@code false}
+     */
+    boolean contains(T value);
+
+    /**
+     * 获取堆的大小
+     * 
+     * @return 堆的大小
+     */
+    int size();
+
+    /**
+     * 将堆中的元素转为集合
+     * 
+     * @return 集合
+     */
+    java.util.Collection<T> toCollection();
+}
---
+++ Heap/src/pers/mingshan/heap/MinHeap.java
@@ -0,0 +1,55 @@
+package pers.mingshan.heap;
+
+import java.util.Collection;
+
+public class MinHeap<T extends Comparable<T>> extends Heap<T> {
+
+    @Override
+    public void add(T value) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public T getHeadValue() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public T remove(T value) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void clear() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public boolean contains(T value) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public Collection<T> toCollection() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void heapify(T[] a, int i, int heapLength) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
