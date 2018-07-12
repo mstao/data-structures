dbdfa949c72c00f6bb57e0761f6b0e31a3bd4f74
Juntao Han <499445428@qq.com>
2018-07-12 22:06:40 +0800

    Update ..

--- Sort/src/me/mingshan/sort/Test.java
+++ Sort/src/me/mingshan/sort/Test.java
@@ -10,10 +10,65 @@ public class Test {
         for (i = 0; i < N; i++) {
             for (j = 0; j < N; j++) {
                 a[i][j] = z;
-                System.out.print(a[i][j]);
                 z++;
             }
-            System.out.print("\n");
         }
+
+        int[] b = clockwisePrint(a, N, N);
+
+        for (i = 0; i < 9; i++) {
+            System.out.println(b[i]);
+        }
+    }
+
+    public static int[] clockwisePrint(int[][] mat, int n, int m) {
+        // 计算数组长度，初始化一维数组
+        int[] result = new int[m*n];
+        int count = 0, hang = 0, lie = 0;
+        int rows = n, cols = m;
+        int index = 0;
+        if (mat.length < 0) { 
+            return null;
+        }
+        while (rows >= 1 && cols >= 1) {
+            if (rows >=1 && cols >= 1) {
+                for (; lie < m - index; lie++) {
+                    result[count++] = mat[hang][lie];
+                }
+                hang++;
+                lie--;
+                rows--;
+            }
+
+            if (rows >= 2 && cols >= 1) {
+                for(; hang<n-index;hang++) {
+                    result[count++]=mat[hang][lie];
+                }
+                lie--;
+                hang--;
+                cols--;
+            }
+
+            if (rows >= 1 && cols >= 1) {
+                for (; lie > index - 1; lie--) {
+                    result[count++] = mat[hang][lie];
+                }
+                hang--;
+                lie++;
+                rows--;
+            }
+
+            if (cols >=1 && rows >= 2) {
+                for (; hang > index; hang--) {
+                    result[count++] = mat[hang][lie];
+                }
+                lie++;
+                hang++;
+                cols--;
+            }
+            index++;
+        }
+
+        return result;
     }
 }
