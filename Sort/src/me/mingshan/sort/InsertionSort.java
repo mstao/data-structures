af79f61f2025b5b08aa2c02007d0a09bba6d651a
Juntao Han <499445428@qq.com>
2018-07-13 19:55:50 +0800

    Add binaryInsertionSort.

--- Sort/src/me/mingshan/sort/InsertionSort.java
+++ Sort/src/me/mingshan/sort/InsertionSort.java
@@ -9,8 +9,9 @@ package me.mingshan.sort;
 public class InsertionSort {
 
     public static void main(String[] args) {
-        int[] a = new int[] {3,1,4,9};
-        insertionSort(a);
+        int[] a = new int[] {3, 8, 0, 1, 4, 9};
+        //insertionSort(a);
+        binaryInsertionSort(a);
         for (int x : a) {
             System.out.println(x);
         }
@@ -21,6 +22,32 @@ public class InsertionSort {
         for (int i = 1; i < len; i++) {
             int j = i;
             while (j > 0 && arr[j] < arr[j-1]) {
+                swap(arr, j, j-1);
+                j--;
+            }
+        }
+    }
+
+    public static void binaryInsertionSort(int[] arr) {
+        int len = arr.length;
+        int low, high, mid;
+        for (int i = 1; i < len; i++) {
+            low = 0;
+            high = i - 1;
+            while (low <= high) {
+                mid = (low + high) / 2;
+                // 判断当前元素在mid位置的左边还是右边
+                if (arr[i] > arr[mid]) {
+                    // 查找右半部分
+                    low = mid + 1;
+                } else {
+                    // 查找左半部分
+                    high = mid - 1;
+                }
+            }
+
+            int j = i;
+            while (j > low && arr[j] < arr[j-1]) {
                 swap(arr, j, j-1);
                 j--;
             }
