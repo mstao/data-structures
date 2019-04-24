8116014f0d32a98ec5e4ce2df8f16608f9b5634c
Juntao Han <499445428@qq.com>
2019-04-24 22:21:34 +0800

    Add Distinct

---
+++ Algorithm/src/me/mingshan/algorithm/distinct/Distinct.java
@@ -0,0 +1,71 @@
+package me.mingshan.algorithm.distinct;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+/**
+ * @author mingshan
+ */
+public class Distinct {
+
+    public static void main(String[] args) {
+        User user1 = new User();
+        user1.setId("1");
+        user1.setName("李华");
+        user1.setCity("郑州");
+        user1.setSchool("郑州大学");
+
+        User user2 = new User();
+        user2.setId("2");
+        user2.setName("李华");
+        user2.setCity("郑州");
+        user2.setSchool("郑州轻工业");
+
+        User user3 = new User();
+        user3.setId("3");
+        user3.setName("小明");
+        user3.setCity("郑州");
+        user3.setSchool("郑州大学");
+
+        User user4 = new User();
+        user4.setId("4");
+        user4.setName("李华");
+        user4.setCity("广州");
+        user4.setSchool("广州大学");
+
+        List<User> users = new ArrayList<>();
+        users.add(user1);
+        users.add(user2);
+        users.add(user3);
+        users.add(user4);
+        List<User> users2 = streamDistinct(users, user -> user.getName() + "," + user.getCity());
+        //users2.stream().forEach(System.out::println);
+
+        ArrayList<User> tempList = new ArrayList<>();
+        tempList.addAll(users);
+        tempList.removeAll(users2);
+        tempList.stream().forEach(System.out::println);
+
+        System.out.println("---");
+        users.stream().forEach(System.out::println);
+    }
+
+    public static <T> List<T> streamDistinct(List<T> source, Function<? super T, ?> keyExtractor) {
+        Objects.requireNonNull(source);
+
+        return source.parallelStream().filter(distinctByKey(keyExtractor))
+                .collect(Collectors.toList());
+    }
+
+    public static <T> Predicate<T> distinctByKey(Function<? super T, ?> keyExtractor) {
+        Objects.requireNonNull(keyExtractor);
+
+        Set<Object> seen = ConcurrentHashMap.newKeySet();
+        return t -> seen.add(keyExtractor.apply(t));
+    }
+
+
+}
---
+++ Algorithm/src/me/mingshan/algorithm/distinct/User.java
@@ -0,0 +1,53 @@
+package me.mingshan.algorithm.distinct;
+
+/**
+ * @author mingshan
+ */
+public class User {
+    private String id;
+    private String name;
+    private String city;
+    private String school;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", city='" + city + '\'' +
+                ", school='" + school + '\'' +
+                '}';
+    }
+}
