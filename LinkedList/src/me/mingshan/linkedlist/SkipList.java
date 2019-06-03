f0a627f88138f0cda2345497d4931a82f5786de8
Juntao Han <499445428@qq.com>
2019-06-03 21:38:26 +0800

    Update SkipList.java

--- LinkedList/src/me/mingshan/linkedlist/SkipList.java
+++ LinkedList/src/me/mingshan/linkedlist/SkipList.java
@@ -3,9 +3,164 @@ package me.mingshan.linkedlist;
 /**
  * 跳表
  * 
- * @author mingshan
- *
  */
-public class SkipList<E> {
+public class SkipList<T> {
 
+    // 最高层数
+    private final int MAX_LEVEL;
+    // 当前层数
+    private int listLevel;
+    // 表头
+    private Node<T> listHead;
+    // 表尾
+    private Node<T> NIL;
+    // 生成randomLevel用到的概率值
+    private final double P;
+    // 论文里给出的最佳概率值
+    private static final double OPTIMAL_P = 0.25;
+
+    public SkipList() {
+        // 0.25, 15
+        this(OPTIMAL_P, (int)Math.ceil(Math.log(Integer.MAX_VALUE) / Math.log(1 / OPTIMAL_P)) - 1);
+    }
+
+    public SkipList(double probability, int maxLevel) {
+        P = probability;
+        MAX_LEVEL = maxLevel;
+
+        listLevel = 1;
+        listHead = new Node<T>(Integer.MIN_VALUE, null, maxLevel);
+        NIL = new Node<T>(Integer.MAX_VALUE, null, maxLevel);
+        for (int i = listHead.forward.length - 1; i >= 0; i--) {
+            listHead.forward[i] = NIL;
+        }
+    }
+
+    // 内部类
+    class Node<T> {
+        int key;
+        T value;
+        Node[] forward;
+        
+        public Node(int key, T value, int level) {
+            this.key = key;
+            this.value = value;
+            this.forward = new Node[level];
+        }
+    }
+
+    public T search(int searchKey) {
+        Node<T> curNode = listHead;
+
+        for (int i = listLevel; i > 0; i--) {
+            while (curNode.forward[i].key < searchKey) {
+                curNode = curNode.forward[i];
+            }
+        }
+
+        if (curNode.key == searchKey) {
+            return curNode.value;
+        } else {
+            return null;
+        }
+    }
+
+    public void insert(int searchKey, T newValue) {
+        Node<T>[] update = new Node[MAX_LEVEL];
+        Node<T> curNode = listHead;
+
+        for (int i = listLevel - 1; i >= 0; i--) {
+            while (curNode.forward[i].key < searchKey) {
+                curNode = curNode.forward[i];
+            }
+            // curNode.key < searchKey <= curNode.forward[i].key
+            update[i] = curNode;
+        }
+
+        curNode = curNode.forward[0];
+
+        if (curNode.key == searchKey) {
+            curNode.value = newValue;
+        } else {
+            int lvl = randomLevel();
+
+            if (listLevel < lvl) {
+                for (int i = listLevel; i < lvl; i++) {
+                    update[i] = listHead;
+                }
+                listLevel = lvl;
+            }
+
+            Node<T> newNode = new Node<T>(searchKey, newValue, lvl);
+
+            for (int i = 0; i < lvl; i++) {
+                newNode.forward[i] = update[i].forward[i];
+                update[i].forward[i] = newNode;
+            }
+        }
+    }
+
+    public void delete(int searchKey) {
+        Node<T>[] update = new Node[MAX_LEVEL];
+        Node<T> curNode = listHead;
+
+        for (int i = listLevel - 1; i >= 0; i--) {
+            while (curNode.forward[i].key < searchKey) {
+                curNode = curNode.forward[i];
+            }
+            // curNode.key < searchKey <= curNode.forward[i].key
+            update[i] = curNode;
+        }
+
+        curNode = curNode.forward[0];
+
+        if (curNode.key == searchKey) {
+            for (int i = 0; i < listLevel; i++) {
+                if (update[i].forward[i] != curNode) {
+                    break;
+                }
+                update[i].forward[i] = curNode.forward[i];
+            }
+
+            while (listLevel > 0 && listHead.forward[listLevel - 1] == NIL) {
+                listLevel--;
+            }
+        }
+    }
+
+    private int randomLevel() {
+        int level = 1;
+        while (level < MAX_LEVEL && Math.random() < P) {
+            level++;
+        }
+        return level;
+    }
+
+    public void print() {
+        for (int i = listLevel - 1; i >= 0; i--) {
+            Node<T> curNode = listHead.forward[i];
+            while (curNode != NIL) {
+                System.out.print(curNode.key + "->");
+                curNode = curNode.forward[i];
+            }
+            System.out.println("NIL");
+        }
+    }
+
+    public static void main(String[] args) {
+        SkipList<Integer> sl = new SkipList<Integer>();
+        sl.insert(20, 20);
+        sl.insert(5, 5);
+        sl.insert(10, 10);
+        sl.insert(1, 1);
+        sl.insert(100, 100);
+        sl.insert(80, 80);
+        sl.insert(60, 60);
+        sl.insert(30, 30);
+        sl.print();
+        System.out.println("---");
+        sl.delete(20);
+        sl.delete(100);
+        sl.print();
+    }
 }
