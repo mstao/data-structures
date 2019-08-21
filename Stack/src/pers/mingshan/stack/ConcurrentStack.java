ab46caa74a154aac24f3fac69714b20e44deaa18
hanjuntao <hanjuntao@hd123.com>
2019-08-21 14:21:14 +0800

    Add ConcurrentStack

---
+++ Stack/src/pers/mingshan/stack/ConcurrentStack.java
@@ -0,0 +1,66 @@
+package pers.mingshan.stack;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * 并发栈
+ *
+ * @param <E>
+ */
+public class ConcurrentStack<E> {
+  AtomicReference<Node<E>> top = new AtomicReference<Node<E>>();
+
+  public void push(E item) {
+    Node<E> newHead = new Node<E>(item);
+    Node<E> oldHead;
+    do {
+      oldHead = top.get();
+      newHead.next = oldHead;
+    } while (!top.compareAndSet(oldHead, newHead));
+  }
+
+  public E pop() {
+    Node<E> oldHead;
+    Node<E> newHead;
+    do {
+      oldHead = top.get();
+      if (oldHead == null) {
+        return null;
+      }
+      newHead = oldHead.next;
+    } while (!top.compareAndSet(oldHead, newHead));
+    return oldHead.item;
+  }
+
+  public boolean isEmpty() {
+    return top.get() == null;
+  }
+
+  public int size() {
+    int currentSize = 0;
+    Node<E> current = top.get();
+    while (current != null) {
+      currentSize++;
+      current = current.next;
+    }
+    return currentSize;
+  }
+
+  public E peek() {
+    Node<E> eNode = top.get();
+    if (eNode == null) {
+      return null;
+    } else {
+      return eNode.item;
+    }
+  }
+
+  private static class Node<E> {
+    E item;
+    Node<E> next;
+
+    Node(E item) {
+      this.item = item;
+    }
+  }
+}
