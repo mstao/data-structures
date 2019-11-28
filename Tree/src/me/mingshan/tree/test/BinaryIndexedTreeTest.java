bc53d7ae4d13c08cb8ae7929526a849499053534
mstao <499445428@qq.com>
2019-11-28 19:12:14 +0800

    Update BinaryIndexedTree test

--- Tree/src/me/mingshan/tree/test/BinaryIndexedTreeTest.java
+++ Tree/src/me/mingshan/tree/test/BinaryIndexedTreeTest.java
@@ -49,4 +49,33 @@ public class BinaryIndexedTreeTest {
     System.out.printf("%3d", bTree.sum(2, 4));
 
   }
+
+  @Test
+  public void test2() {
+    int k1 = 6; // 0000 0110
+    // 取反 11111001
+    // + 1  11111010
+    //   &  00000111
+    // 结果 00000010
+
+    System.out.println(lowBit(k1)); // 0000 0010
+
+    int k2 = 10; // 0000 1010
+    System.out.println(lowBit(k2)); // 0000 0010
+
+    int k3 = 12; // 0000 1100
+    System.out.println(lowBit(k3)); // 0000 0100
+
+    int k4 = 14; // 0000 1110;
+    System.out.println(lowBit(k4));
+
+    int k5 = 15; // 0000 1111;
+    System.out.println(lowBit(k5)); // 0000 0001
+
+  }
+
+
+  private static int lowBit(int k) {
+    return k & -k;
+  }
 }
