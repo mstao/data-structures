c089e4b6d04708fc1d6a1a054563d691c6296927
mstao <walkerhan@126.com>
2019-11-30 18:30:58 +0800

    Add ReverseInteger

--- leetcode/code/src/me/mingshan/leetcode/L_7_ReverseInteger.java
+++ leetcode/code/src/me/mingshan/leetcode/L_7_ReverseInteger.java
@@ -1,5 +1,7 @@
 package me.mingshan.leetcode;
 
+import java.util.Arrays;
+
 /**
  * 整数反转
  *
@@ -9,11 +11,40 @@ package me.mingshan.leetcode;
  */
 public class L_7_ReverseInteger {
   public static void main(String[] args) {
+    int x = 111221;
 
+    System.out.println(reverse(x));
   }
 
-  public int reverse(int x) {
 
-    return 0;
+  public static int reverse(int x) {
+    if (x == 0)
+      return 0;
+    
+    String source = String.valueOf(x);
+    boolean f = false;
+    if (source.contains("-")) {
+      f = true;
+      source = source.replace("-", "");
+    }
+    char[] chars = source.toCharArray();
+    for (int i = 0; i < chars.length / 2; i++) {
+      char temp = chars[i];
+      chars[i] = chars[chars.length - i -1];
+      chars[chars.length - i -1] = temp;
+    }
+
+    int result;
+    try {
+      result = Integer.parseInt(String.valueOf(chars));
+    } catch (NumberFormatException e) {
+      return 0;
+    }
+
+    if (f) {
+      return -result;
+    }
+
+    return result;
   }
 }
