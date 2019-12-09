33d5fd85f28eb616e139e83d45187fee3a5bd3d4
mstao <walkerhan@126.com>
2019-12-09 21:35:51 +0800

    Add reverse

--- Algorithm/src/me/mingshan/algorithm/divide/QuickSort.java
+++ Algorithm/src/me/mingshan/algorithm/divide/QuickSort.java
@@ -1,4 +1,26 @@
 package me.mingshan.algorithm.divide;
 
+/**
+ * 快速排序
+ *
+ * @author mingshan
+ *
+ */
 public class QuickSort {
+
+    public static void sort(int[] source, int low , int height) {
+
+    }
+
+    /**
+     * 交换
+     *
+     * @param a
+     * @param b
+     */
+    public static void swap(int a, int b) {
+        int temp = a;
+        a = b;
+        b = a;
+    }
 }
