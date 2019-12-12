435886656497e26d0f373d026d295c24219424f1
mstao <499445428@qq.com>
2019-12-12 11:29:00 +0800

    Update CollectionDivision

--- Algorithm/src/me/mingshan/algorithm/divide/CollectionDivision.java
+++ Algorithm/src/me/mingshan/algorithm/divide/CollectionDivision.java
@@ -1,21 +1,31 @@
 package me.mingshan.algorithm.divide;
 
 /**
- * 集合划分
+ * 集合划分【第二类Stirling数】
  *
  * F（n,m）表示把n个元素的集合分为m个子集，有多少种分法？
+ *
+ * 通项公式：
+ * F(n,m) = F(n-1,m-1) + m * F(n-1,m)
  *
  * @author mingshan
  */
 public class CollectionDivision {
 
   public static void main(String[] args) {
-    int[] source = {1, 4, 3, 5, 9, 7};
-    System.out.println(solution(source, 4));
+    System.out.println(divide(30, 2));
   }
 
-  public static int solution(int[] source, int m) {
-    return 0;
+  public static long divide(int n, int m) {
+    if (m > n) {
+      return 0;
+    }
+
+    if (m == 1 || m == n) {
+      return 1;
+    }
+
+    return divide(n-1,m-1)+m*divide(n-1, m);
   }
 
 }
