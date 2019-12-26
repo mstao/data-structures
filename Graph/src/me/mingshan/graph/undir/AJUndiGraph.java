25926fbfa779437caa89e3cefbb5e8833bf02aea
mstao <499445428@qq.com>
2019-12-26 17:08:38 +0800

    Update Graph

---
+++ Graph/src/me/mingshan/graph/undir/AJUndiGraph.java
@@ -0,0 +1,14 @@
+package me.mingshan.graph.undir;
+
+/**
+ * 无向图 - 基于邻接表(adjacency list)
+ *
+ * @author hanjuntao
+ */
+public class AJUndiGraph {
+  // 节点数目
+  private int nodeSize;
+  // 边数量
+  private int edgeSize;
+
+}
