760b9c5c5910c32067d911fd087eb6dc3e2291b3
mstao <499445428@qq.com>
2019-12-27 10:03:17 +0800

    Update AJUndiGraph

--- Graph/src/me/mingshan/graph/undir/AJUndiGraph.java
+++ Graph/src/me/mingshan/graph/undir/AJUndiGraph.java
@@ -9,28 +9,42 @@ import java.util.LinkedList;
  *
  * @author hanjuntao
  */
-public class AJUndiGraph<E> implements Graph {
+public class AJUndiGraph implements Graph {
   // 邻接表
-  private LinkedList<E>[] adj;
+  private LinkedList<Integer>[] adj;
   // 节点数目
   private int nodeSize;
   // 边数量
   private int edgeSize;
 
-
-  public AJUndiGraph(int nodeSize) {
-    if (nodeSize <= 0) {
-      throw new IllegalArgumentException("The nodeSize [" + nodeSize + "] is not valid number");
+  public AJUndiGraph(int arrSize) {
+    if (arrSize <= 0) {
+      throw new IllegalArgumentException("The arrSize [" + arrSize+ "] is not valid number");
     }
 
-    for (int i = 0; i < nodeSize; i++) {
-
+    adj = new LinkedList[arrSize];
+    for (int i = 0; i < arrSize; i++) {
+      adj[i] = new LinkedList<>();
     }
+  }
+
+  public void addEdge(int start, int end) {
+    adj[start].add(end);
+    adj[end].add(start);
+    this.edgeSize++;
   }
 
   @Override
   public int getNodeSize() {
-    return 0;
+    int currNodeSize = adj.length;
+
+    for (LinkedList list : adj) {
+      if (list != null) {
+        currNodeSize += list.size();
+      }
+    }
+
+    return currNodeSize;
   }
 
   @Override
