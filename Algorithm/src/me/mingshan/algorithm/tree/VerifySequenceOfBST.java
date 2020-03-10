e75e67e78b540b50b4781eb83f01b65718f42b62
mstao <walkerhan@126.com>
2020-03-10 22:50:23 +0800

    Add VerifySequenceOfBST

---
+++ Algorithm/src/me/mingshan/algorithm/tree/VerifySequenceOfBST.java
@@ -0,0 +1,11 @@
+package me.mingshan.algorithm.tree;
+
+/**
+ * 输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历的结果。如果是则返回true，否则返回 false。假设输入的数组的任意两个数字都互不相同
+ */
+public class VerifySequenceOfBST {
+
+  public void solution(int[] data) {
+
+  }
+}
