2a62e7f30ec2bdb2ef110ee5f6e12e441b943a42
mstao <499445428@qq.com>
2020-03-12 12:01:54 +0800

    Add BinarySearchTreeConvertList

---
+++ Algorithm/src/me/mingshan/algorithm/tree/BinarySearchTreeConvertList.java
@@ -0,0 +1,21 @@
+package me.mingshan.algorithm.tree;
+
+import me.mingshan.tree.BinarySearchTree;
+
+/**
+ * 输入一棵二叉搜索树，将该二叉搜索树转换成一个排序的双向链表。要求不能创建任何新的结点，只能调整树中结点指针的指向
+ */
+public class BinarySearchTreeConvertList {
+
+  public static BinarySearchTree.Node<String> solution(BinarySearchTree.Node<String> root) {
+    if (root == null)
+      return null;
+
+    BinarySearchTree.Node<String> node = root;
+
+    while (node != null) {
+
+    }
+    return null;
+  }
+}
