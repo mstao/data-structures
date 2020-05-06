8d3e3c7660c50068db5f01c9031afa2fd5ae09c3
mstao <walkerhan@126.com>
2020-05-06 20:38:04 +0800

    Add MinimumWindowSubstring

--- Algorithm/src/me/mingshan/algorithm/slidingwindow/MinimumWindowSubstring.java
+++ Algorithm/src/me/mingshan/algorithm/slidingwindow/MinimumWindowSubstring.java
@@ -1,5 +1,8 @@
 package me.mingshan.algorithm.slidingwindow;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 给定一个字符串 S 和一个字符串 T，请在 S 中找出包含 T 所有字母的最小子串。(minimum-window-substring)
  *
@@ -9,11 +12,53 @@ package me.mingshan.algorithm.slidingwindow;
 public class MinimumWindowSubstring {
 
   public static void main(String[] args) {
-
+    String s = "ADOBECODEBANC";
+    String t = "ABC";
+    System.out.println(solution(s, t));
   }
 
   public static String solution(String s, String t) {
+    if (s == null || t == null || s.length() < t.length()) {
+      return null;
+    }
 
-    return s;
+    Map<Character, Integer> map = new HashMap<>();
+    for (int i = 0; i < t.length(); i++) {
+      char c = t.charAt(i);
+      map.merge(c, 1, Integer::sum);
+    }
+
+    int left = 0;
+    int right = 0;
+    int count = t.length();
+    int max = Integer.MAX_VALUE;
+    String res = s;
+
+    while (right < s.length()) {
+      char r = s.charAt(right);
+      if (map.get(r) != null && map.get(r) > 0) {
+        count--;
+      }
+
+      if (map.containsKey(r)) {
+        map.put(r, map.get(r) - 1);
+      }
+      right++;
+
+      while (count == 0) {
+        if (right - left < max) {
+          max = right - left;
+          res = s.substring(left, right);
+        }
+        char le = s.charAt(left);
+        map.merge(le, 1, Integer::sum);
+        if (map.get(le) != null && map.get(le) > 0) {
+          count++;
+        }
+        left++;
+      }
+    }
+
+    return max == Integer.MAX_VALUE ? "" : res;
   }
 }
