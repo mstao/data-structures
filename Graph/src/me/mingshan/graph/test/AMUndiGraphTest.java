efd0cb55deb0a21161b9165ac8bbdfe6743b5bd9
mstao <walkerhan@126.com>
2020-07-20 23:27:10 +0800

    Add bfs dfs

--- Graph/src/me/mingshan/graph/test/AMUndiGraphTest.java
+++ Graph/src/me/mingshan/graph/test/AMUndiGraphTest.java
@@ -30,4 +30,30 @@ public class AMUndiGraphTest {
 
     System.out.println(graph.toString());
   }
+
+  @Test
+  public void bfs() {
+    AMUndiGraph graph = new AMUndiGraph(4);
+
+    graph.addEdge(0, 1);
+    graph.addEdge(0, 3);
+    graph.addEdge(1, 2);
+    graph.addEdge(2, 3);
+
+    graph.bfs(0, 2);
+  }
+
+  @Test
+  public void dfs() {
+    AMUndiGraph graph = new AMUndiGraph(4);
+
+    graph.addEdge(0, 1);
+    graph.addEdge(0, 3);
+    graph.addEdge(1, 2);
+    graph.addEdge(2, 3);
+
+    graph.dfs(0, 2);
+  }
+
+
 }
--- Graph/src/me/mingshan/graph/undir/AMUndiGraph.java
+++ Graph/src/me/mingshan/graph/undir/AMUndiGraph.java
@@ -3,6 +3,8 @@ package me.mingshan.graph.undir;
 import me.mingshan.graph.Graph;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * 无向图 - 基于邻接矩阵(adjacency matrix)
@@ -50,6 +52,99 @@ public class AMUndiGraph implements Graph {
     this.adjacencyMatrix[start][end] = 1;
     this.adjacencyMatrix[end][start] = 1;
     this.edgeSize++;
+  }
+
+  /**
+   * 广度优先搜索 Breadth-First-Search
+   *
+   * {@code
+   *
+   * void bfs (传入起始结点的位置）
+   *
+   * {
+   *
+   *         定义队列 q
+   *
+   *         配置、压入首节点，并把首节点pop出队列
+   *
+   *         访问标记
+   *
+   *    while(！q.empty（）)
+   *
+   *    {
+   *
+   *         取出队列头结点，作为本次处理的点对象。
+   *
+   *         检验：此点是否已经达到目标状态，如果达到，直接结束bfs。（记住：检验在扩展之前）
+   *
+   *         如果没达到，开始从这个点发散扩展，并将扩展找到的所有合法结点，压入队列末尾。
+   *
+   *    }
+   *
+   *    return ;
+   *
+   * }
+   * }
+   *
+   * @param s 起始顶点
+   * @param t 终止顶点
+   */
+  public void bfs(int s, int t) {
+    boolean[] visited = new boolean[sideLength];
+    Queue<Integer> queue = new LinkedList<>();
+    queue.add(s);
+    visited[s] = true;
+
+    while (!queue.isEmpty()) {
+      Integer item = queue.poll();
+
+      // 访问其邻居
+      for (int i = 0; i < sideLength; i++) {
+        int curr = this.adjacencyMatrix[item][i];
+        if (curr != 1) {
+          continue;
+        }
+
+        if (!visited[i]) {
+          System.out.println(i);
+
+          if (i == t) {
+            return;
+          }
+
+          queue.add(i);
+        }
+      }
+    }
+  }
+
+  /**
+   * 深度优先搜索 Depth-First-Search
+   *
+   * @param s 起始顶点
+   * @param t 终止顶点
+   */
+  public void dfs(int s, int t) {
+    boolean[] visited = new boolean[sideLength];
+    recurDfs(s, t, visited);
+  }
+
+  private void recurDfs(int vertex, int t, boolean[] visited) {
+    visited[vertex] = true;
+    if (vertex == t) {
+      return;
+    }
+
+    for (int i = 0; i < sideLength; ++i) {
+      int curr = adjacencyMatrix[vertex][i];
+      if (curr != 1) {
+        continue;
+      }
+      if (!visited[i]) {
+        System.out.println("当前顶点：" + vertex + "，当前节点：" + i);
+        recurDfs(i, t, visited);
+      }
+    }
   }
 
   /**
