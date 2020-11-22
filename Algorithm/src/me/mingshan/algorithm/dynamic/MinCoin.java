5d749c1a606d349b3b0101bb823c5f51d42d2d25
mstao <walkerhan@126.com>
2020-11-22 21:29:15 +0800

    Refine code

--- Algorithm/src/me/mingshan/algorithm/dynamic/MinCoin.java
+++ Algorithm/src/me/mingshan/algorithm/dynamic/MinCoin.java
@@ -2,7 +2,6 @@ package me.mingshan.algorithm.dynamic;
 
 /**
  * 现有 2元，5元，七元三种硬币，假设硬币都足够多，先求解：最少用多少枚上述硬币拼出27块钱
- *
  */
 public class MinCoin {
 
@@ -34,27 +33,34 @@ public class MinCoin {
     return res;
   }
 
-
-  public static int solution2(int[] a, int m) {
-    int[] f = new int[m + 1];
+  /**
+   * 运用动态规划解决
+   *
+   * @param coins  硬币的种类
+   * @param amount 要拼出的钱数
+   * @return 最少硬币数
+   */
+  public static int solution2(int[] coins, int amount) {
+    // f[x]  代表多少枚硬币拼出x
+    int[] f = new int[amount + 1];
     f[0] = 0;
 
     // 1,2..27
-    for (int i = 1; i <= m; ++i) {
+    for (int i = 1; i <= amount; i++) {
       f[i] = Integer.MAX_VALUE;
 
-      for (int j = 0; j < a.length; ++j) {
+      for (int j = 0; j < coins.length; j++) {
         // 当前的硬币值小于要拼出的数值
-        if (i >= a[j] && f[i - a[j]] != Integer.MAX_VALUE) {
-          f[i] = Math.min(f[i - a[j]] + 1, f[i]);
+        if (i >= coins[j] && f[i - coins[j]] != Integer.MAX_VALUE) {
+          f[i] = Math.min(f[i - coins[j]] + 1, f[i]);
         }
       }
     }
 
-    if (f[m] == Integer.MAX_VALUE) {
+    if (f[amount] == Integer.MAX_VALUE) {
       return -1;
     }
 
-    return f[m];
+    return f[amount];
   }
 }
