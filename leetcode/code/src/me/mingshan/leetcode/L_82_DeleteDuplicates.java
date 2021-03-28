d1bf2ee485ba37c204a80240b8b27ba2d8aa7b15
mstao <walkerhan@126.com>
2021-03-28 23:41:07 +0800

    Update L_剑指Offer03_FindRepeatNumber

--- leetcode/code/src/me/mingshan/leetcode/L_82_DeleteDuplicates.java
+++ leetcode/code/src/me/mingshan/leetcode/L_82_DeleteDuplicates.java
@@ -10,11 +10,39 @@ package me.mingshan.leetcode;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class L_82_DeleteDuplicates {
+  /**
+   * 思路：
+   *
+   * 我们可以利用已有的集合空间，结合两个指针，一个指针i一直指向非重复元素的最后一位，一个指针j用来向前探测重复的元素，
+   * 当j遇到一个新的元素时，就将其位置的元素拷贝到i的下一位，同时i自增，指向元素，然后j跳过该元素的重复区间，
+   * 继续探测下一个不同的元素，直至列表末尾，最后输出0~i这个区间的元素即可。
+   *
+   *
+   * @param head
+   * @return
+   */
   public ListNode deleteDuplicates(ListNode head) {
     if (head == null) {
       return null;
     }
 
+    ListNode curr = head;
 
+    ListNode iNode = curr;
+    ListNode jNode = curr;
+
+    while (head.next != null) {
+      
+    }
+
+    return head;
+  }
+
+  public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
   }
 }
