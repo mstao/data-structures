3209f175356966bc0789e24f6c793716abc7c3bc
mstao <walkerhan@126.com>
2021-03-28 22:35:50 +0800

    Update L_240_SearchMatrix

--- leetcode/code/src/me/mingshan/leetcode/L_240_SearchMatrix.java
+++ leetcode/code/src/me/mingshan/leetcode/L_240_SearchMatrix.java
@@ -35,9 +35,69 @@ public class L_240_SearchMatrix {
 //    int[][] matrix6 = {{1,4},{2,5}};
 //    System.out.println(searchMatrix(matrix6, 5)); // true
 
-    int[][] matrix7 = {{5,6,10,14},{6,10,13,18},{10,13,18,19}};
-    System.out.println(searchMatrix(matrix7, 14)); // true
+//    int[][] matrix7 = {{5,6,10,14},{6,10,13,18},{10,13,18,19}};
+//    System.out.println(searchMatrix3(matrix7, 14)); // true
+
+//    int[][] matrix8 = {{-5}};
+//    System.out.println(searchMatrix3(matrix8, -5));
+
+    int[][] matrix9 = {{1,1}};
+    System.out.println(searchMatrix3(matrix9, 2));
   }
+
+  /**
+   * 整体思路是尽可能多的排除掉无关 行/列 ，可以从 第一排最后一个/第一列最后一个 开始搜索，这里选择从 第一排最后一个 开始
+   * 由于行列规则等价，搜索策略 先按行排除/按列排除 也是等价的，这里选择 按行排除
+   * 搜索规则：小于 target 则向左搜索，大于 则向下搜索，可以保证 global search
+   * 若超出 矩阵大小 则意味着没有匹配 target，输出 False
+   *
+   * 作者：eloise-1
+   * 链接：https://leetcode-cn.com/problems/search-a-2d-matrix-ii/solution/si-lu-qing-xi-zhe-xian-sou-suo-by-eloise-kisj/
+   * 来源：力扣（LeetCode）
+   * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   *
+   * @param matrix
+   * @param target
+   * @return
+   */
+  public static boolean searchMatrix3(int[][] matrix, int target) {
+    if (matrix == null || matrix[0].length == 0) {
+      return false;
+    }
+
+    int m = matrix.length;
+    int n = matrix[0].length;
+
+    int currRow = 0;
+    int currCol = n - 1;
+
+    while (currRow < m && currCol >= 0) {
+      if (matrix[currRow][currCol] == target) {
+        return true;
+      }
+
+      // 当前值大于target，向左进行搜索
+      if (matrix[currRow][currCol] > target) {
+        currCol--;
+      } else {
+        // 当前值小于target，向下进行搜索
+        currRow++;
+      }
+    }
+
+    return false;
+  }
+
+
+
+
+
+
+
+
+
+
+
 
   public static boolean searchMatrix(int[][] matrix, int target) {
     if (matrix == null || matrix.length == 0) {
---
+++ leetcode/code/src/me/mingshan/leetcode/L_82_DeleteDuplicates.java
@@ -0,0 +1,20 @@
+package me.mingshan.leetcode;
+
+/**
+ * 存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除链表中所有存在数字重复情况的节点，只保留原始链表中 没有重复出现 的数字。
+ *
+ * 返回同样按升序排列的结果链表。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class L_82_DeleteDuplicates {
+  public ListNode deleteDuplicates(ListNode head) {
+    if (head == null) {
+      return null;
+    }
+
+
+  }
+}
