bbd0e39e820caf9f21c634758c88c9b45b41d0a6
mstao <walkerhan@126.com>
2021-04-01 00:04:37 +0800

    Update L_26_RemoveDuplicates

--- leetcode/code/src/me/mingshan/leetcode/L_26_RemoveDuplicates.java
+++ leetcode/code/src/me/mingshan/leetcode/L_26_RemoveDuplicates.java
@@ -7,6 +7,12 @@ import java.util.Arrays;
  *
  * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
  *
+ * 示例 1：
+ *
+ * 输入：nums = [1,1,2]
+ * 输出：2, nums = [1,2]
+ * 解释：函数应该返回新的长度 2 ，并且原数组 nums 的前两个元素被修改为 1, 2 。不需要考虑数组中超出新长度后面的元素。
+ *
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
