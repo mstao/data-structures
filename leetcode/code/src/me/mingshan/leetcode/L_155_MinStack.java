3522e58d1a36d08addeff9512e3a36c9a7c432dd
Juntao Han <499445428@qq.com>
2021-04-13 20:10:54 +0800

    Update L_155_MinStack

--- leetcode/code/src/me/mingshan/leetcode/L_155_MinStack.java
+++ leetcode/code/src/me/mingshan/leetcode/L_155_MinStack.java
@@ -2,23 +2,20 @@ package me.mingshan.leetcode;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.Stack;
 
 /**
  * 155. 最小栈
- *
+ * <p>
  * 设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
- *
+ * <p>
  * push(x) —— 将元素 x 推入栈中。
  * pop() —— 删除栈顶的元素。
  * top() —— 获取栈顶元素。
  * getMin() —— 检索栈中的最小元素。
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/min-stack
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
- *
  */
 public class L_155_MinStack {
 
@@ -43,7 +40,9 @@ public class L_155_MinStack {
 
   private List<Integer> dataStack;
 
-  /** initialize your data structure here. */
+  /**
+   * initialize your data structure here.
+   */
   public L_155_MinStack() {
     dataStack = new ArrayList<>();
   }
