fda434a30bdd92c95ee10ea16d19753b4d0d9ea7
Juntao Han <499445428@qq.com>
2021-04-25 00:07:40 +0800

    Update L_234_IsPalindrome.java

--- leetcode/code/src/me/mingshan/leetcode/L_234_IsPalindrome.java
+++ leetcode/code/src/me/mingshan/leetcode/L_234_IsPalindrome.java
@@ -26,14 +26,15 @@ public class L_234_IsPalindrome {
 
   public static void main(String[] args) {
     ListNode head = new ListNode(1);
-    ListNode node2 = new ListNode(2);
-    ListNode node3 = new ListNode(2);
+    ListNode node2 = new ListNode(1);
+    ListNode node3 = new ListNode(1);
     ListNode node4 = new ListNode(1);
 
     head.next = node2;
-    node2.next = node3;
+//    node2.next = node3;
+//    node3.next = node4;
 
-    System.out.println(isPalindrome(head));
+    System.out.println(isPalindrome2(head));
   }
 
   /**
@@ -69,14 +70,86 @@ public class L_234_IsPalindrome {
     return true;
   }
 
+  /**
+   * 不使用额外空间
+   *
+   * @param head
+   * @return
+   */
   public static boolean isPalindrome2(ListNode head) {
-    if (head == null) {
+    if (head == null || head.next == null) {
       return true;
+    }
+
+    // 先统计总节点数
+    int nodeCount = 0;
+
+    ListNode curr = head;
+    while (curr != null) {
+      nodeCount++;
+      curr = curr.next;
+    }
+
+    // 将从中间节点链表反转
+
+    int mid = nodeCount % 2 == 0 ? nodeCount / 2 : nodeCount / 2 + 1;
+
+    curr = head;
+    ListNode midNode = null;
+    int currCount = 0;
+
+    while (curr != null) {
+      currCount++;
+
+      if (currCount == mid) {
+        midNode = curr;
+        break;
+      }
+
+      curr = curr.next;
+    }
+
+    // 从两头向中间遍历，依次比较
+    ListNode reverseHead = reverse(midNode);
+
+    ListNode reverseNode = reverseHead;
+    curr = head;
+
+    int compareCount = 0;
+    while (true) {
+      compareCount++;
+      if (reverseNode.val != curr.val) {
+        return false;
+      }
+
+      if (compareCount == mid) {
+        break;
+      }
+
+      reverseNode = reverseNode.next;
+      curr = curr.next;
     }
 
     return true;
   }
 
+  /**
+   * 反转链表
+   *
+   * @param midNode
+   * @return
+   */
+  private static ListNode reverse(ListNode midNode) {
+    if (midNode == null || midNode.next == null) {
+      return midNode;
+    }
+
+    ListNode node = reverse(midNode.next);
+    midNode.next.next = midNode;
+    midNode.next = null;
+    return node;
+  }
+  
   static class ListNode {
     int val;
     ListNode next;
