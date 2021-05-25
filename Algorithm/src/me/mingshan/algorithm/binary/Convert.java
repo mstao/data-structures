d464a779e6a6c6e1add69d663ccc1eb660c3a142
Juntao Han <499445428@qq.com>
2021-05-25 23:35:07 +0800

    Update Convert

--- Algorithm/src/me/mingshan/algorithm/binary/Convert.java
+++ Algorithm/src/me/mingshan/algorithm/binary/Convert.java
@@ -1,5 +1,6 @@
 package me.mingshan.algorithm.binary;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.Stack;
 
@@ -15,8 +16,24 @@ public class Convert {
 
   public static void main(String[] args) {
     System.out.println(convert(14, 2));
+
+    System.out.println(convert10("0xACDC233"));
   }
 
+  /**
+   * 十进制转任意进制，
+   *
+   * 处理方式：
+   *
+   * 十进制整数转换为二进制整数十进制整数转换为二进制整数采用"除2取余，逆序排列"法。
+   * 具体做法是：用2整除十进制整数，可以得到一个商和余数；再用2去除商，又会得到一个商和余数，
+   * 如此进行，直到商为小于1时为止，然后把先得到的余数作为二进制数的低位有效位，后得到的余数作为二进制数的高位有效位，依次排列起来。
+   *
+   *
+   * @param n
+   * @param base
+   * @return
+   */
   public static String convert(int n, int base) {
     Stack<Character> data = new Stack<>();
     while (n > 0) {
@@ -34,5 +51,72 @@ public class Convert {
     }
 
     return Arrays.toString(result);
+  }
+
+  /**
+   * 将十六进制转为10进制
+   *
+   * @param source 十六进制数如：
+   * @return
+   */
+  public static Long convert10(String source) {
+    if (source == null || source.length() == 0) {
+      return null;
+    }
+
+    if (source.startsWith("0x")) {
+      source = source.substring(2);
+    }
+
+    if (source.length() == 0) {
+      return null;
+    }
+
+    long result = 0;
+
+    char[] chars = source.toCharArray();
+    int length = chars.length;
+    int i = length - 1;
+
+    while (i >= 0) {
+      char aChar = chars[i];
+
+      int bit = toInteger(aChar);
+      // 将char转化为数字
+
+      result += bit * Math.pow(16, length - 1 - i);
+      i--;
+    }
+
+    return result;
+  }
+
+  private static int toInteger(char aChar) {
+    String str = String.valueOf(aChar);
+    if ("A".equals(str)) {
+      return 10;
+    }
+
+    if ("B".equals(str)) {
+      return 11;
+    }
+
+    if ("C".equals(str)) {
+      return 12;
+    }
+
+    if ("D".equals(str)) {
+      return 13;
+    }
+
+    if ("E".equals(str)) {
+      return 14;
+    }
+
+    if ("F".equals(str)) {
+      return 15;
+    }
+
+    return Integer.parseInt(str);
   }
 }
