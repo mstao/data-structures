6fad169a6af702f54c64cb669020ca86ff2892d2
Juntao Han <499445428@qq.com>
2021-05-28 23:23:07 +0800

    Update

---
+++ leetcode/code/src/me/mingshan/leetcode/B.java
@@ -0,0 +1,16 @@
+package me.mingshan.leetcode;
+
+public class B {
+
+  public static void main(String[] args) throws InterruptedException {
+    f();
+
+    int x = 5;
+
+    System.out.println( x= 6);
+  }
+
+  private static void f() {
+
+  }
+}
