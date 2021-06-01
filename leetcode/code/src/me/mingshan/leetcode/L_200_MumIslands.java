f5be4f3f295e267a8d848d77990ea836d0dc50d2
Juntao Han <499445428@qq.com>
2021-06-01 20:26:54 +0800

    Update L_200_MumIslands.java

--- leetcode/code/src/me/mingshan/leetcode/L_200_MumIslands.java
+++ leetcode/code/src/me/mingshan/leetcode/L_200_MumIslands.java
@@ -1,5 +1,8 @@
 package me.mingshan.leetcode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  *
  * 200. 岛屿数量
@@ -22,6 +25,8 @@ package me.mingshan.leetcode;
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/number-of-islands
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * 参考：https://mp.weixin.qq.com/s/it_asGIDqQBoOrzdL1j2CQ
  */
 public class L_200_MumIslands {
 
@@ -33,9 +38,23 @@ public class L_200_MumIslands {
         {'0', '0', '0', '0', '0'}
     };
 
-    System.out.println(numIslands(grid));
+    //System.out.println(numIslands2(grid));
+
+    char[][] grid2 = {
+        {'1','1','1'},
+        {'0','1','0'},
+        {'1','1','1'},
+    };
+    //System.out.println(numIslands(grid2));
+    System.out.println(numIslands2(grid2));
   }
 
+  /**
+   * 利用dfs
+   *
+   * @param grid
+   * @return
+   */
   public static int numIslands(char[][] grid) {
     if (grid == null || grid.length == 0 || grid[0].length == 0) {
       return 0;
@@ -52,7 +71,7 @@ public class L_200_MumIslands {
     for (int i = 0; i < rowNum; i++) {
       for (int j = 0; j < colNum; j++) {
         if (grid[i][j] == '1') {
-          find(grid, i, j);
+          dfs(grid, i, j);
           result++;
         }
       }
@@ -68,7 +87,7 @@ public class L_200_MumIslands {
    * @param i 行
    * @param j 列
    */
-  private static void find(char[][] grid, int i, int j) {
+  private static void dfs(char[][] grid, int i, int j) {
     // 行数
     int rowNum = grid.length;
     // 列数
@@ -80,10 +99,86 @@ public class L_200_MumIslands {
 
     grid[i][j] = '0';
 
-    find(grid, i, j + 1);
-    find(grid, i, j - 1);
-    find(grid, i + 1, j);
-    find(grid, i - 1, j);
+    dfs(grid, i, j + 1);
+    dfs(grid, i, j - 1);
+    dfs(grid, i + 1, j);
+    dfs(grid, i - 1, j);
+  }
+
+  /**
+   * 利用bfs，广度优先遍历, 上下左右依次遍历即可
+   *
+   *
+   *
+   * @param grid
+   * @return
+   */
+  public static int numIslands2(char[][] grid) {
+    if (grid == null || grid.length == 0 || grid[0].length == 0) {
+      return 0;
+    }
+
+    // 行数
+    int rowNum = grid.length;
+    // 列数
+    int colNum = grid[0].length;
+    int result = 0;
+
+    Queue<Position> queue = new LinkedList<>();
+
+    for (int i = 0; i < rowNum; i++) {
+      for (int j = 0; j < colNum; j++) {
+        if (grid[i][j] == '1') {
+          result++;
+          grid[i][j] = '0';
+
+          queue.add(new Position(i, j));
+          while (!queue.isEmpty()) {
+            Position position = queue.poll();
+
+            int row = position.row;
+            int col = position.col;
+
+            // 上
+            if (row - 1 >= 0 && grid[row - 1][col] == '1') {
+              queue.add(new Position(row - 1, col));
+              grid[row - 1][col] = '0';
+            }
+
+            // 下
+            if (row + 1 < rowNum && grid[row + 1][col] == '1') {
+              queue.add(new Position(row + 1, col));
+              grid[row + 1][col] = '0';
+            }
+
+            // 左
+            if (col - 1 >= 0 && grid[row][col - 1] == '1') {
+              queue.add(new Position(row, col - 1));
+              grid[row][col - 1] = '0';
+            }
+
+            // 右
+            if (col + 1 < colNum && grid[row][col + 1] == '1') {
+              queue.add(new Position(row, col + 1));
+              grid[row][col + 1] = '0';
+            }
+
+          }
+        }
+      }
+    }
+
+    return result;
+  }
+
+  private static final class Position {
+    int row;
+    int col;
+
+    public Position(int row, int col) {
+      this.row = row;
+      this.col = col;
+    }
   }
 
 }
