7880179e17448ef08648bcf3c09077716c269db6
Walker Han <walkerhan@126.com>
2022-01-15 16:54:40 +0800

    Add L_33_Search

--- leetcode/code/src/me/mingshan/leetcode/L_33_Search.java
+++ leetcode/code/src/me/mingshan/leetcode/L_33_Search.java
@@ -2,26 +2,120 @@ package me.mingshan.leetcode;
 
 /**
  * 整数数组 nums 按升序排列，数组中的值 互不相同 。
- *
+ * <p>
  * 在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转，使数组变为 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。例如， [0,1,2,4,5,6,7] 在下标 3 处经旋转后可能变为 [4,5,6,7,0,1,2] 。
- *
+ * <p>
  * 给你 旋转后 的数组 nums 和一个整数 target ，如果 nums 中存在这个目标值 target ，则返回它的下标，否则返回-1。
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/search-in-rotated-sorted-array
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ *
+ * 数组的旋转
+ * https://leetcode-cn.com/problems/xuan-zhuan-shu-zu-de-zui-xiao-shu-zi-lcof/
  *
  * @author hanjuntao
  * @date 2022/1/14
  */
 public class L_33_Search {
-    public int search(int[] nums, int target) {
+
+    public static void main(String[] args) {
+        int[] nums1 = {4, 5, 6, 7, 0, 1, 2};
+
+        //System.out.println(search2(nums1, 6));
+
+        int[] nums2 = {4, 5, 6, 7, 8, 0, 1};
+
+        //System.out.println(search2(nums2, 1));
+
+        int[] nums3 = {1, 3};
+
+        //System.out.println(search2(nums3, 4));
+
+        int[] nums4 = {4, 5, 6, 7, 8, 1, 2,3};
+
+        System.out.println(search2(nums4, 8));
+    }
+
+    public static int search(int[] nums, int target) {
         if (nums == null || nums.length == 0) {
             return -1;
         }
 
         int result = -1;
 
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return i;
+            }
+        }
+
         return result;
+    }
+
+    /**
+     * 二分
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int search2(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+
+        return search2(nums, target, 0, nums.length - 1);
+    }
+
+    public static int search2(int[] nums, int target, int begin, int end) {
+        if (begin == end) {
+            return nums[begin] == target ? begin : -1;
+        }
+
+        if (end - begin == 1) {
+            if (nums[begin] == target) {
+                return begin;
+            }
+            if (nums[end] == target) {
+                return end;
+            }
+
+            return -1;
+        }
+
+        int mid = (begin + end) / 2;
+
+        if (nums[mid] == target) {
+            return mid;
+        }
+
+        int first = nums[0];
+        int last = nums[nums.length - 1];
+
+        // 开启条件：begin 的值大于等于 first
+
+        // 如果目标值，小于第一个数，那么必然也要从右边区域寻找
+        // 例如：4,5,6,7,8,0,1 target = 1
+        if (nums[begin] >= first && first > target) {
+            return search2(nums, target, mid + 1, end);
+        }
+
+        // 开启条件：
+
+        // 如果目标值，小于第一个数，那么必然也要从左边区域寻找
+        // 例如： 6,7,0,1,3  target = 6
+        if (nums[end] <= last && last < target) {
+            return search2(nums, target, begin, mid - 1);
+        }
+
+        if (nums[mid] > target) {
+            return search2(nums, target, begin, mid - 1);
+        } else if (nums[mid] < target) {
+            return search2(nums, target, mid + 1, end);
+        }
+
+        return -1;
     }
 }
--- leetcode/code/src/me/mingshan/leetcode/L_剑指Offer11_MinArray.java
+++ leetcode/code/src/me/mingshan/leetcode/L_剑指Offer11_MinArray.java
@@ -2,9 +2,9 @@ package me.mingshan.leetcode;
 
 /**
  * 剑指 Offer 11. 旋转数组的最小数字
- *
+ * <p>
  * 把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。输入一个递增排序的数组的一个旋转，输出旋转数组的最小元素。例如，数组 [3,4,5,1,2] 为 [1,2,3,4,5] 的一个旋转，该数组的最小值为1。  
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/xuan-zhuan-shu-zu-de-zui-xiao-shu-zi-lcof
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
@@ -14,125 +14,142 @@ package me.mingshan.leetcode;
  */
 public class L_剑指Offer11_MinArray {
 
-  public static void main(String[] args) {
-    int[] num1 = {4,5,1,2,3};
-    System.out.println(minArray(num1));
+    public static void main(String[] args) {
+        int[] num1 = {4, 5, 1, 2, 3};
+        System.out.println(minArray(num1));
 
-    int[] num3 = {1,2,3,4,5};
-    System.out.println(minArray(num3));
+        int[] num3 = {1, 2, 3, 4, 5};
+        System.out.println(minArray(num3));
 
-    int[] num4 = {3,4,5,1,2};
-    System.out.println(minArray(num4));
+        int[] num4 = {3, 4, 5, 1, 2};
+        System.out.println(minArray(num4));
 
-    int[] num5 = {2,2,2,0,1};
-    System.out.println(minArray(num5));
+        int[] num5 = {2, 2, 2, 0, 1};
+        System.out.println(minArray(num5));
 
-    int[] num6 = {3,3,3,1};
-    System.out.println(minArray(num6));
+        int[] num6 = {3, 3, 3, 1};
+        System.out.println(minArray(num6));
 
-    int[] num7 = {2,2,0,0,1,1};
-    System.out.println(minArray(num7));
+        int[] num7 = {2, 2, 0, 0, 1, 1};
+        System.out.println(minArray(num7));
 
-    int[] num8 = {1,1,2,2,0,0};
-    System.out.println(minArray(num8));
+        int[] num8 = {1, 1, 2, 2, 0, 0};
+        System.out.println(minArray(num8));
 
-    int[] num9 = {2,2,0,0,0,1,1,1};
-    System.out.println(minArray(num9));
+        int[] num9 = {2, 2, 0, 0, 0, 1, 1, 1};
+        System.out.println(minArray(num9));
 
-    int[] num10 = {10,10,10,10,1};
-    System.out.println(minArray(num10));
+        int[] num10 = {10, 10, 10, 10, 1};
+        System.out.println(minArray(num10));
 
 
-    int[] num11 = {0,1,2,2};
-    System.out.println(minArray(num11));
+        int[] num11 = {0, 1, 2, 2};
+        System.out.println(minArray(num11));
 
-    int[] num12 = { 2,2,2,0,1};
-    System.out.println(minArray(num12));
-  }
-
-  /**
-   * 直接for循环
-   *
-   * @param numbers
-   * @return
-   */
-  public static int minArray2(int[] numbers) {
-    if (numbers == null || numbers.length == 0) {
-      throw new IllegalArgumentException("numbers is empty");
+        int[] num12 = {2, 2, 2, 0, 1};
+        System.out.println(minArray3(num12));
     }
 
-    if (numbers.length == 1) {
-      return numbers[0];
+    /**
+     * 直接for循环
+     *
+     * @param numbers
+     * @return
+     */
+    public static int minArray2(int[] numbers) {
+        if (numbers == null || numbers.length == 0) {
+            throw new IllegalArgumentException("numbers is empty");
+        }
+
+        if (numbers.length == 1) {
+            return numbers[0];
+        }
+
+        int lastVal = numbers[0];
+
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] < lastVal) {
+                return numbers[i];
+            }
+
+            lastVal = numbers[i];
+        }
+
+        return numbers[0];
     }
 
-    int lastVal = numbers[0];
+    /**
+     * 二分
+     * <p>
+     * 逻辑：
+     *
+     * @param numbers
+     * @return
+     */
+    public static int minArray(int[] numbers) {
+        if (numbers == null || numbers.length == 0) {
+            throw new IllegalArgumentException("numbers is empty");
+        }
 
-    for (int i = 1; i < numbers.length; i++) {
-      if (numbers[i] < lastVal) {
-        return numbers[i];
-      }
+        if (numbers.length == 1) {
+            return numbers[0];
+        }
 
-      lastVal = numbers[i];
+        Integer min = findMin(numbers, 0, numbers.length - 1);
+        if (min != null) {
+            return min;
+        }
+
+        return numbers[0];
     }
 
-    return numbers[0];
-  }
+    private static Integer findMin(int[] numbers, int low, int high) {
+        int mid = (low + high) / 2;
 
-  /**
-   * 二分
-   *
-   * @param numbers
-   * @return
-   */
-  public static int minArray(int[] numbers) {
-    if (numbers == null || numbers.length == 0) {
-      throw new IllegalArgumentException("numbers is empty");
+        if (low > high) {
+            return null;
+        }
+
+        if (low == mid || high == mid) {
+            if (numbers[low] > numbers[high]) {
+                return numbers[high];
+            }
+        }
+
+        if ((mid - 1) >= 0 && (mid + 1) <= high) {
+            // 左小（等）右小
+            if (numbers[mid - 1] <= numbers[mid] && numbers[mid + 1] < numbers[mid]) {
+                return numbers[mid + 1];
+            }
+
+            // 左大右大（等）
+            if (numbers[mid - 1] > numbers[mid] && numbers[mid + 1] >= numbers[mid]) {
+                return numbers[mid];
+            }
+
+            Integer min1 = findMin(numbers, mid + 1, high);
+            if (min1 != null) {
+                return min1;
+            }
+
+            return findMin(numbers, low, mid - 1);
+        }
+
+        return null;
     }
 
-    if (numbers.length == 1) {
-      return numbers[0];
+    public static int minArray3(int[] numbers) {
+        int l = 0, r = numbers.length - 1;
+        while (l < r) {
+            int mid = ((r - l) >> 1) + l;
+            //只要右边比中间大，那右边一定是有序数组
+            if (numbers[r] > numbers[mid]) {
+                r = mid;
+            } else if (numbers[r] < numbers[mid]) {
+                l = mid + 1;
+                //去重
+            } else r--;
+        }
+        return numbers[l];
     }
-
-    Integer min = findMin(numbers, 0, numbers.length - 1);
-    if (min != null) {
-      return min;
-    }
-
-    return numbers[0];
-  }
-
-  private static Integer findMin(int[] numbers, int low, int high) {
-    int mid = (low + high) / 2;
-
-    if (low > high) {
-      return null;
-    }
-
-    if (low == mid || high == mid) {
-      if (numbers[low] > numbers[high]) {
-        return numbers[high];
-      }
-    }
-
-    if ((mid - 1) >= 0 && (mid + 1) <= high) {
-      // 左小（等）右小
-      if (numbers[mid-1] <= numbers[mid] && numbers[mid+1] < numbers[mid]) {
-        return numbers[mid+1];
-      }
-
-      // 左大右大（等）
-      if (numbers[mid-1] > numbers[mid] && numbers[mid+1] >= numbers[mid]) {
-        return numbers[mid];
-      }
-
-      Integer min1 = findMin(numbers, mid + 1, high);
-      if (min1 != null) {
-        return min1;
-      }
-
-      return findMin(numbers, low, mid - 1);
-    }
-
-    return null;
-  }
 }
